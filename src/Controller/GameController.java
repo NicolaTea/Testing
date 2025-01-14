4fbaa8791dbefff2ec79414ac8fe3ae012233e5a
NicolaTea <teanicola2004@gmail.com>
2025-01-14 13:42:32 +0200

    updated gamecontroller

--- src/Controller/GameController.java
+++ src/Controller/GameController.java
@@ -4,9 +4,7 @@ import Repo.InMemoryRepo;
 import model.Trophy;
 import model.TrophyTypes;
 import model.VideoGame;
-
 import java.util.List;
-import java.util.function.Predicate;
 
 public class GameController {
     private final InMemoryRepo<VideoGame> videoGameRepo;
@@ -18,16 +16,22 @@ public class GameController {
         this.trophyRepo = trophyRepo;
     }
 
-    //CRUD VideoGame
+    // CRUD VideoGame
     public void addGame(String name) {
         VideoGame newGame = new VideoGame(name);
-        videoGameRepo.add(newGame);
+        boolean added = videoGameRepo.add(newGame, VideoGame::getName);
+        if (added) {
+            System.out.println("Game '" + name + "' added successfully.");
+        }
     }
 
     public void deleteGame(String name) {
         VideoGame gameToDelete = videoGameRepo.find(game -> game.getName().equals(name)).stream().findFirst().orElse(null);
         if (gameToDelete != null) {
             videoGameRepo.delete(gameToDelete);
+            System.out.println("Game '" + name + "' deleted successfully.");
+        } else {
+            System.out.println("Game '" + name + "' not found.");
         }
     }
 
@@ -36,17 +40,24 @@ public class GameController {
         if (gameToUpdate != null) {
             gameToUpdate.setName(newName);
             videoGameRepo.update(gameToUpdate, game -> game.getName().equals(oldName));
+            System.out.println("Game '" + oldName + "' updated to '" + newName + "'.");
+        } else {
+            System.out.println("Game '" + oldName + "' not found.");
         }
     }
 
-
-    //CRUD Trophy
+    // CRUD Trophy
     public void addTrophyToGame(String gameName, String title, String description, TrophyTypes type) {
         VideoGame game = videoGameRepo.find(vg -> vg.getName().equals(gameName)).stream().findFirst().orElse(null);
         if (game != null) {
             Trophy newTrophy = new Trophy(title, description, type);
-            game.getTrophyList().add(newTrophy);
-            trophyRepo.add(newTrophy);
+            boolean added = trophyRepo.add(newTrophy, Trophy::getTitle);
+            if (added) {
+                game.getTrophyList().add(newTrophy);
+                System.out.println("Trophy '" + title + "' added to game '" + gameName + "'.");
+            }
+        } else {
+            System.out.println("Game '" + gameName + "' not found.");
         }
     }
 
@@ -57,7 +68,12 @@ public class GameController {
             if (trophyToDelete != null) {
                 game.getTrophyList().remove(trophyToDelete);
                 trophyRepo.delete(trophyToDelete);
+                System.out.println("Trophy '" + title + "' removed from game '" + gameName + "'.");
+            } else {
+                System.out.println("Trophy '" + title + "' not found in game '" + gameName + "'.");
             }
+        } else {
+            System.out.println("Game '" + gameName + "' not found.");
         }
     }
 
@@ -69,7 +85,12 @@ public class GameController {
                 trophyToUpdate.setTitle(newTitle);
                 trophyToUpdate.setDescription(newDescription);
                 trophyToUpdate.setType(newType);
+                System.out.println("Trophy '" + oldTitle + "' updated to '" + newTitle + "'.");
+            } else {
+                System.out.println("Trophy '" + oldTitle + "' not found in game '" + gameName + "'.");
             }
+        } else {
+            System.out.println("Game '" + gameName + "' not found.");
         }
     }
 
@@ -79,13 +100,5 @@ public class GameController {
 
     public List<Trophy> getAllTrophies() {
         return trophyRepo.getAll();
-    }
-
-    public List<VideoGame> findGames(Predicate<VideoGame> condition) {
-        return videoGameRepo.find(condition);
-    }
-
-    public List<Trophy> findTrophies(Predicate<Trophy> condition) {
-        return trophyRepo.find(condition);
     }
 }
