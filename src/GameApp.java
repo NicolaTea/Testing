7cacbe331d79d3daa7b50c10a64e4508a263e0fd
NicolaTea <teanicola2004@gmail.com>
2025-01-14 10:34:07 +0200

    add sorting+filter methods+javadocs

--- src/GameApp.java
+++ src/GameApp.java
@@ -2,15 +2,65 @@ import java.io.*;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class GameApp {
     public static void main(String[] args){
 
     }
 
+    /**
+     * sort games by metaCritic
+     * @param games
+     * @return
+     */
+    public static List<Game> sortbyMetacritic(List<Game> games){
+        return games.stream()
+                .sorted(Comparator.comparing(Game::getMetaCritic).reversed())
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * sort games by releaseDate
+     * @param games
+     * @return
+     */
+    public static List<Game> sortbyReleaseDate(List<Game> games){
+        return games.stream()
+                .sorted(Comparator.comparing(Game::getReleaseDate).reversed())
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * filter games by UserScore
+     * @param games
+     * @return
+     */
+    public static List<Game> filterbyUserScore(List<Game> games){
+        return games.stream()
+                .filter(game -> game.getUserScore()>9)
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * filter games by name
+     * @param games
+     * @return
+     */
+    public static List<Game> filterbyName(List<Game> games){
+        return games.stream()
+                .filter(game -> game.getName().startsWith("D"))
+                .collect(Collectors.toList());
+    }
 
 
+    /**
+     * read the file
+     * @param filename
+     * @return
+     */
     private static List<Game> loadGameFromFile(String filename){
         List<Game> games=new ArrayList<>();
         try(BufferedReader reader=new BufferedReader(new FileReader(filename))){
@@ -33,6 +83,11 @@ public class GameApp {
         return  games;
     }
 
+    /**
+     * write to file
+     * @param games
+     * @param filename
+     */
     private static void writeGameToFile(List<Game> games,String filename){
         try (PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(filename)))) {
             for (Game game : games) {
